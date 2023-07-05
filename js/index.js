ed1647bd532a799dbc54fff10a584f4d1042a46b
SinghDevanshh <ds.singhdevansh@gmail.com>
2023-07-05 18:22:21 +0530

    All Code Uploaded

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+        function copyText() {
+            // Get the anchor tag element
+            var anchorTag = document.querySelector('.email-link');
+
+            // Get the text inside the anchor tag
+            var text = anchorTag.innerText.trim();
+
+            // Create a temporary input element
+            var tempInput = document.createElement('input');
+            tempInput.setAttribute('value', text);
+            document.body.appendChild(tempInput);
+
+            // Copy the text from the input element
+            tempInput.select();
+            document.execCommand('copy');
+            document.body.removeChild(tempInput);
+
+            // Alert the user that the text has been copied
+            alert('Text copied: ' + text);
+        }
